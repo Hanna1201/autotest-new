ddc4c5ea83d0815cba3284d5830f12891d77c89c
anna <hannakavaliova1201@gmail.com>
2026-03-14 12:11:59 +0300

    Add new class task trecker

---
+++ AutoProjectJava/src/TaskTracker.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TaskTracker {
+
+
+
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите свое имя: ");
+    String userName = scanner.nextLine();
+
+     //   System.out.println("Привет: " + userName);
+}
+
